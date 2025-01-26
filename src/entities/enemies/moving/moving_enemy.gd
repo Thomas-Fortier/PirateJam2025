9c76f5ebd693cf81795f5a3e7c5f3fe0d5914281
Jonathan-Caro <JohnnySoulresident@hotmail.com>
2025-01-26 14:15:07 -0500

    Fixed moving enemy gaining speed

--- src/entities/enemies/moving/moving_enemy.gd
+++ src/entities/enemies/moving/moving_enemy.gd
@@ -1,13 +1,14 @@
 class_name MovingEnemy
 extends Enemy
 
-const speed = 30
-var dir: Vector2
+@export var speed: float = 3000
+@export var movement_interval: Array[float] = [1.0, 1.5, 2.0]
+var _movement_direction: Vector2
 
 func _ready() -> void:
 	add_to_group("enemies")
 
-func _process(delta):
+func _physics_process(delta: float) -> void:
 	move(delta)
 
 func _on_death() -> void:
@@ -15,18 +16,11 @@ func _on_death() -> void:
 	queue_free()
 
 ## Make the enemy move in a random direction
-func move(delta):
-	# TODO: Implement logic to prevent enemy from gaining speed if the same direction is chosen twice in a row
-	velocity += dir * speed * delta
+func move(delta: float) -> void:
+	velocity = _movement_direction * speed * delta
 	move_and_slide()
 
 ## Function to choose random wait time and direction to move
-func _on_timer_timeout():
-	$Timer.wait_time = choose([1.0, 1.5, 2.0])
-	dir = choose([Vector2.RIGHT, Vector2.LEFT, Vector2.UP, Vector2.DOWN])
-	print(dir)
-
-## Helper function to randomize array
-func choose(array):
-	array.shuffle()
-	return array.front()
+func _on_timer_timeout() -> void:
+	$Timer.wait_time = movement_interval.pick_random()
+	_movement_direction = [Vector2.RIGHT, Vector2.LEFT, Vector2.UP, Vector2.DOWN].pick_random()
