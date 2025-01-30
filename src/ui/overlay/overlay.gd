3f1e4d6ad887bc1643d006109a6efb131afd207d
Thomas Fortier <thomasfortier.cs@gmail.com>
2025-01-29 20:48:17 -0500

    Changed overlay to use custom theme

--- src/ui/overlay/overlay.gd
+++ src/ui/overlay/overlay.gd
@@ -6,9 +6,6 @@ extends Control
 @onready var _ricochets_label: Label = %RicochetsLabel
 @onready var _turns_label: Label = %TurnsLabel
 
-const CRITICAL_COLOR = Color.RED
-const NORMAL_COLOR = Color("bffff1")
-
 func _ready() -> void:
 	_points_label.text = str(StatsManager.points)
 	StatsManager.points_changed.connect(_on_points_changed)
@@ -26,12 +23,10 @@ func _on_points_changed(points: int) -> void:
 ## Logic to execute when the ricochet counter has changed.
 func _on_ricochets_changed(ricochets_remaining: int) -> void:
 	_ricochets_label.text = str(ricochets_remaining)
-	_ricochets_label.label_settings.font_color = CRITICAL_COLOR if ricochets_remaining == 0 else NORMAL_COLOR
 
 ## Logic that is executed when the turns have been changed.
 func _on_turns_changed(turns: int, max_turns: int) -> void:
 	_turns_label.text = _get_turns_text(turns, max_turns)
-	_turns_label.label_settings.font_color = CRITICAL_COLOR if turns <= 1 else NORMAL_COLOR
 
 ## Gets the turn count as a fraction string.
 func _get_turns_text(turns: int, max_turns: int) -> String:
