7f8904dfdd2ae101ede1cbfae201898a45f87ca3
Thomas Fortier <thomasfortier.cs@gmail.com>
2025-02-12 12:42:48 -0500

    Removed unused signals

--- src/ui/popups/pause_menu/pause_menu.gd
+++ src/ui/popups/pause_menu/pause_menu.gd
@@ -1,9 +1,6 @@
 class_name PauseMenu
 extends UserInterface
 
-var is_sub_menu_opened: bool = false
-var _settings_menu_instance: SettingsMenu = null
-
 func _ready():
 	size.x = 640
 	size.y = 360
@@ -13,15 +10,8 @@ func _on_resume_button_pressed() -> void:
 	close_window()
 
 func _on_settings_button_pressed():
-	_settings_menu_instance = UiManager.show_settings_screen()
-	_settings_menu_instance.settings_menu_closed.connect(_on_settings_menu_closed)
-	is_sub_menu_opened = true
+	UiManager.show_settings_screen()
 
 func _on_quit_button_pressed():
 	GameManager.quit_game()
 	close_window()
-
-func _on_settings_menu_closed() -> void:
-	_settings_menu_instance.settings_menu_closed.disconnect(_on_settings_menu_closed)
-	_settings_menu_instance = null
-	is_sub_menu_opened = false
--- src/ui/popups/settings/settings.gd
+++ src/ui/popups/settings/settings.gd
@@ -1,12 +1,6 @@
 class_name SettingsMenu
 extends UserInterface
 
-signal settings_menu_opened
-signal settings_menu_closed
-
-func _ready():
-	settings_menu_opened.emit()
-
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		close_window()
